d73b776afa6b8f4b1b2a5e2dbc8473f2d38e6da0
Luis Fernando <luisfernandobega@gmail.com>
2023-04-20 14:18:55 -0300

    create users table

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,18 @@
 const AppError = require('../utils/AppError')
-
-const sqliteConnnection = require('../database/sqlite')
+const knex = require('../database/knex')
 
 class UsersControllers {
+  async create(request, response) {
+    const { name, email, password } = request.body
 
-  create (request, response)  {
-      const {name, email, password} = request.body
+    const checkUserExists = await knex('users').where({ email })
 
-
+    if(checkUserExists) {
+      throw new AppError('Este email ja esta em uso')
     }
+
+    return response.status(201).json
   }
+}
 
 module.exports = UsersControllers
---
+++ src/database/sqlite/migrations/index.js
@@ -0,0 +1,14 @@
+const sqliteConnnection = require('../../sqlite')
+const createUsers = require('./createUsers')
+
+async function migrationsRun() {
+  const schemas = [
+    createUsers,
+  ].join('')
+
+  sqliteConnnection()
+  .then(db => db.exec(schemas)
+  .catch(error => console.log(error)))
+}
+
+module.exports = migrationsRun

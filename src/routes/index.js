38777253e6fd389d80a101d4fed615686efb6e8c
Luis Fernando <luisfernandobega@gmail.com>
2023-04-28 14:43:38 -0300

    Tags Controller Done

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express')
 
 const usersRouter = require('./users.routes')
 const noteRouter = require('./notes.routes')
+const tagRouter = require('./tags.routes')
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/notes', noteRouter)
+routes.use('/tags', tagRouter)
 
 module.exports = routes

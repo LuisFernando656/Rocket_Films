bcd718e502def7f396a697d9659eece96b53e11e
Luis Fernando <luisfernandobega@gmail.com>
2023-07-28 11:02:11 -0300

    sessions routes created

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require('express')
 const usersRouter = require('./users.routes')
 const noteRouter = require('./notes.routes')
 const tagRouter = require('./tags.routes')
+const sessionsRouter = require('./sessions.routes')
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
 routes.use('/notes', noteRouter)
 routes.use('/tags', tagRouter)
 
